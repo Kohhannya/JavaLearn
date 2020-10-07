b8fcab40023d9be33e64c109358ab6155ffe79f0
Lubasha <lubochka2002@gmail.com>
2020-10-07 12:40:48 +0300

    chngs

--- Homework1.java
+++ Homework1.java
@@ -9,7 +9,8 @@ public class Homework1 {
     //0.5б - если посчитаете в цикле
     //1б - если посчитаете рекурсией
     public static int fact(int n){
-        if (n == 2) return n;
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
         return fact(n - 1) * n;
     }
 
